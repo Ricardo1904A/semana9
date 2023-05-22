9488bf622d4a46822478469ebc90171a5089e662
Laboratorio <laboratorio@udla-ec.int>
2023-05-22 16:24:52 -0500

    Codigo Final

--- Matriz Transpuesta.c
+++ Matriz Transpuesta.c
@@ -13,4 +13,32 @@ int main()
     scanf("%d", &c);
     srand(time(NULL));
     int matriz[f][c];
+
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            matriz[i][j] = rand() % 100; // Genera un número aleatorio entre 0 y 99
+        }
+    }
+
+    printf("La matriz generada con numeros aleatorios es: \n"); // Imprimir matriz generada
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("La matriz inversa es:\n");
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", matriz[j][i]);
+        }
+        printf("\n");
+    }
     return 0;
+}
